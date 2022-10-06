dc3bfae6147911e2b18220912317ab9e82319c04
FOR-TIMI <timiplayskeys@gmail.com>
2022-10-06 06:21:44 -0400

    added register feature

--- public/js/register.js
+++ public/js/register.js
@@ -1,14 +1,16 @@
-async function loginFormHandler(event) {
+async function registerHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#loginUsername').value.trim();
-    const password = document.querySelector('#loginPassword').value.trim();
+    const username = document.querySelector('#registerUsername').value.trim();
+    const password = document.querySelector('#registerPassword').value.trim();
+    const email = document.querySelector('#registerEmail').value.trim();
 
     if (username && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({
                 username,
+                email,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
@@ -17,11 +19,11 @@ async function loginFormHandler(event) {
         if (response.ok) {
             document.location.replace('/');
         } else {
-            const container = document.querySelector('#login-container');
+            const container = document.querySelector('#register-container');
             const div = document.createElement('div');
             div.innerHTML = `<div class="alert alert-danger d-flex align-items-center alert-dismissible fade show container col-6" role="alert">
 			<svg class="bi flex-shrink-0 me-2 mr-2" width="20" height="20" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-			Invalid username or password
+			That username already exists
 			<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 		   </div>`
           
@@ -30,6 +32,6 @@ async function loginFormHandler(event) {
         }
     }
 };
-const loginForm = document.querySelector('#login-form')
+const signUpForm = document.querySelector('#register-form')
 
-loginForm.addEventListener('submit', loginFormHandler);
+signUpForm.addEventListener('submit', registerHandler);
